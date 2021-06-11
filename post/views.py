b0af9675cba474c151bc8680785246f53f15fcf5
Tipe123 <Tipe123>
2021-06-11 15:25:23 +0200

    Done with crud

---
+++ post/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render,redirect
+# from django.views.generic.list import ListView
+
+# from django.views.generic.detail import DetailView
+# from django.views.generic.edit import CreateView , UpdateView,DeleteView,FormView
+from django.views.generic import ListView,DetailView,CreateView , UpdateView,DeleteView,FormView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django import forms
+from .models import Product
+# Create your views here.
+
+
+class ProductList(ListView):
+	model = Product
+	context_object_name = 'products'
+	template_name = 'post/product_list.html'
+
+class ProductDetail(DetailView):
+	model = Product
+	context_object_name = 'product'
+	template_name = 'post/product_detail.html'
+
+class ProductCreate(LoginRequiredMixin,CreateView):
+	model = Product
+	fields = "__all__"
+	success_url = reverse_lazy('products')
+	template_name = 'post/product_create.html'
+
+	def form_valid(self,form):
+		form.instance.user = self.request.user
+		return super(ProductCreate , self).form_valid(form)
+
+class ProductEdit(UpdateView):
+	model = Product
+	fields = "__all__"
+	success_url = reverse_lazy('products')
+	template_name = 'post/product_create.html'
+
+class DeleteProduct(DeleteView):
+	model = Product
+	context_object_name = 'product'
+	success_url = reverse_lazy('products')
+	template_name ='post/product_conform_delete.html'
+
+class Fridge(ProductList):
+	template_name = 'post/includes/fridge.html'
+
+class Laptop(ProductList):
+	template_name = 'post/includes/laptop.html'
+
+class Microoven(ProductList):
+	template_name = 'post/includes/microoven.html'
+
+class Stove(ProductList):
+	template_name = 'post/includes/stove.html'
